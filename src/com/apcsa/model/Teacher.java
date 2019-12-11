a269d56eb0d9b247d5ef16b3f05de7c9bcdb1a9f
Corvin Terzo <cterzo@ucvts.tec.nj.us>
2019-12-11 08:21:38 -0500

    added teacher consturctor

--- src/com/apcsa/model/Teacher.java
+++ src/com/apcsa/model/Teacher.java
@@ -1,12 +1,24 @@
 package com.apcsa.model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import com.apcsa.model.User;
 
 public class Teacher extends User {
-
-    private int teacherId;
+	private int teacherId;
     private int departmentId;
     private String firstName;
     private String lastName;
-
+    
+    public Teacher(User user, ResultSet rs) throws SQLException {
+    	super(user.getUserId(), user.getAccountType(), user.getUsername(), user.getPassword(), user.getLastLogin());
+    	
+    	this.teacherId = rs.getInt("teacher_id");
+    	this.departmentId = rs.getInt("department_id");
+    	this.firstName = rs.getString("first_name");
+    	this.lastName = rs.getString("last_name");
+	}
+	
+    
 }
