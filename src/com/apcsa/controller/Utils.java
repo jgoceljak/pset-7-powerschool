e65a46df94dc5cf7c055c66c115015ffe4604caf
Corvin Terzo <cterzo@ucvts.tec.nj.us>
2019-12-13 09:10:23 -0500

    added grade calculator

--- src/com/apcsa/controller/Utils.java
+++ src/com/apcsa/controller/Utils.java
@@ -110,4 +110,61 @@ public class Utils {
                 
         return students;
     }
+    
+    /**
+     * Computes a grade based on marking period grades and exam grades.
+     * 
+     * @param grades a list of grades
+     * @return the final grade
+     */
+
+    public static Double getGrade(Double[] grades) {
+        int mps = 0;
+        double mpSum = 0;
+        double mpAvg = -1;
+        double mpWeight = -1;
+
+        int exams = 0;
+        double examSum = 0;
+        double examAvg = -1;
+        double examWeight = -1;
+        
+        // compute sume of marking period and/or exam grades
+        
+        for (int i = 0; i < grades.length; i++) {
+            if (grades[i] != null) {
+                if (i < 2 || (i > 2 && i < 5)) {        // marking period grade
+                    mps++;
+                    mpSum = mpSum + grades[i];
+                } else {                                // midterm or final exam grade
+                    exams++;
+                    examSum = examSum + grades[i];
+                }
+            }
+        }
+        
+        // compute weights and averages based on entered grades
+        
+        if (mps > 0 && exams > 0) {
+            mpAvg = mpSum / mps;
+            examAvg = examSum / exams;
+             
+            mpWeight = 0.8;
+            examWeight = 0.2;
+        } else if (mps > 0) {
+            mpAvg = mpSum / mps;
+            
+            mpWeight = 1.0;
+            examWeight = 0.0;
+        } else if (exams > 0) {
+            examAvg = examSum / exams;
+            
+            mpWeight = 0.0;
+            examWeight = 1.0;
+        } else {
+            return null;
+        }
+                                
+        return round(mpAvg * mpWeight + examAvg * examWeight, 2);
+    }
 }
