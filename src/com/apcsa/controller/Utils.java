47746b85b1e638c51bc64a9465c243df34db4530
Corvin Terzo <cterzo@ucvts.tec.nj.us>
2019-12-13 09:09:25 -0500

    added class rank ultil method

--- src/com/apcsa/controller/Utils.java
+++ src/com/apcsa/controller/Utils.java
@@ -72,4 +72,42 @@ public class Utils {
         
         return response.equals("y");
     }
+    
+    /**
+     * Sorts the list of students by rank, using the index to update the underlying class rank.
+     * 
+     * @param students the list of students
+     * @return the updated list of students
+     */
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public static ArrayList<Student> updateRanks(ArrayList<Student> students) {
+        Collections.sort(students, new Comparator() {
+
+            // compares each student based on gpa to aid sorting
+            
+            @Override
+            public int compare(Object student1, Object student2) {
+                if (((Student) student1).getGpa() > ((Student) student2).getGpa()) {
+                    return -1;
+                } else if (((Student) student1).getGpa() == ((Student) student2).getGpa()) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            }
+            
+        });
+        
+        // applies a class rank (provided the student has a measurable gpa)
+        
+        int rank = 1;
+        for (int i = 0; i < students.size(); i++) {
+            Student student = students.get(i);
+            
+            student.setClassRank(student.getGpa() != -1 ? rank++ : 0);
+        }
+                
+        return students;
+    }
 }
